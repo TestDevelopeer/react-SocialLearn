2f194b64b33aa9805a9cecf079f0741b48ecb64e
Igor <testdeveloper7967@gmail.com>
2020-08-20 22:51:44 +0300

    11. Вёрстка, grid

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import Header from './Header.js';
-import Technologies from './Technologies.js';
+import './App.css';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Technologies />
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/8/85/Logo-Test.png" alt="Logo" />
+      </header>
+      <nav className="nav">
+        <div><a href="#">Profile</a></div>
+        <div><a href="#">Messages</a></div>
+        <div><a href="#">News</a></div>
+        <div><a href="#">Music</a></div>
+        <div><a href="#">Settings</a></div>
+      </nav>
+      <div className="content">
+        <div>
+          <img src="https://knowpathology.com.au/app/uploads/2018/07/Happy-Test-Screen-01-825x510.png" alt="Content" />
+        </div>
+        <div>
+          avatar + description
+        </div>
+        <div>
+          My posts
+          <div>
+            New Post
+          </div>
+          <div>
+            Post 1
+          </div>
+          <div>
+            Post 2
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
