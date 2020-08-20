fa95cbc5a15ee9fdb820e2d218f53715688f8ab6
Igor <testdeveloper7967@gmail.com>
2020-08-20 22:20:46 +0300

    07. Компонента

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -5,15 +5,31 @@ import './App.css';
 const App = () => {
   return (
     <div>
-      <div>One Div</div>
-      <div className="App">
-        <ul>
-          <li>css</li>
-          <li>html</li>
-          <li>js</li>
-          <li>react</li>
-        </ul>
-      </div>
+      <Header />
+      <Technologies />
+    </div>
+  );
+}
+
+const Technologies = () => {
+  return (
+    <div>
+      <ul>
+        <li>css</li>
+        <li>html</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
+    </div>
+  );
+}
+
+const Header = () => {
+  return (
+    <div>
+      <a href="#">Home</a>
+      <a href="#">News</a>
+      <a href="#">Messages</a>
     </div>
   );
 }
