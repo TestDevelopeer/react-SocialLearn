8621ae77ae5344ac79b706b5b84770959a9a2e7b
Igor <testdeveloper7967@gmail.com>
2020-08-23 18:40:00 +0300

    21. Верстаем страницу Диалогов

--- react-tutorial/src/components/Dialogs/Dialogs.jsx
+++ react-tutorial/src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,32 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Test1
+                </div>
+                <div className={s.dialog}>
+                    Test2
+                </div>
+                <div className={s.dialog}>
+                    Test3
+                </div>
+                <div className={s.dialog}>
+                    Test4
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    Msg1
+                </div>
+                <div className={s.message}>
+                    Msg2
+                </div>
+                <div className={s.message}>
+                    Msg3
+                </div>
+            </div>
         </div>
     );
 }
