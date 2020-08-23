acadc583aa7bd2a960b167500a9c73441ab616f9
Igor <testdeveloper7967@gmail.com>
2020-08-23 18:18:49 +0300

    19. route, browser-router

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -4,17 +4,20 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import { Route, BrowserRouter } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <div className="app-wrapper-content">
-        <Dialogs />
-        {/* <Profile/> */}
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div className="app-wrapper-content">
+          <Route path="/dialogs" component={Dialogs} />
+          <Route path="/profile" component={Profile} />
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- react-tutorial/src/components/Navbar/Navbar.jsx
+++ react-tutorial/src/components/Navbar/Navbar.jsx
@@ -4,11 +4,11 @@ import s from './Navbar.module.css';
 const Navbar = () => {
     return (
         <nav className={s.nav}>
-            <div className={s.item}>Profile</div>
-            <div className={`${s.item} ${s.active}`}>Messages</div>
-            <div className={s.item}>News</div>
-            <div className={s.item}>Music</div>
-            <div className={s.item}>Settings</div>
+            <div className={s.item}><a href="/profile">Profile</a></div>
+            <div className={`${s.item} ${s.active}`}><a href="/dialogs">Messages</a></div>
+            <div className={s.item}><a href="">News</a></div>
+            <div className={s.item}><a href="">Music</a></div>
+            <div className={s.item}><a href="">Settings</a></div>
         </nav>
     );
 }
