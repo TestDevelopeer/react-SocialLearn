f74405fac020c6b57676511d4a1c1057595eb00e
Igor <testdeveloper7967@gmail.com>
2020-08-24 12:33:41 +0300

    27. render вместо component

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -13,8 +13,8 @@ const App = () => {
         <Header />
         <Navbar />
         <div className="app-wrapper-content">
-          <Route path="/dialogs" component={Dialogs} />
-          <Route path="/profile" component={Profile} />
+          <Route path="/dialogs" render={() => <Dialogs />} />
+          <Route path="/profile" render={() => <Profile />} />
         </div>
       </div>
     </BrowserRouter>
