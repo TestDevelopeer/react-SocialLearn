3672033109f394bf38c9bd3e312dcb84773d42ae
Igor <testdeveloper7967@gmail.com>
2020-08-24 13:22:13 +0300

    30.

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -4,24 +4,22 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 const App = (props) => {
   return (
-    <BrowserRouter>
-      <div className="app-wrapper">
-        <Header />
-        <Navbar />
-        <div className="app-wrapper-content">
-          <Route path="/dialogs" render={
-            () => <Dialogs state={props.state.messagesPage}/>
-          } />
-          <Route path="/profile" render={
-            () => <Profile state={props.state.profilePage}/>
-          } />
-        </div>
+    <div className="app-wrapper">
+      <Header />
+      <Navbar />
+      <div className="app-wrapper-content">
+        <Route path="/dialogs" render={
+          () => <Dialogs state={props.state.messagesPage} />
+        } />
+        <Route path="/profile" render={
+          () => <Profile state={props.state.profilePage} />
+        } />
       </div>
-    </BrowserRouter>
+    </div>
   );
 }
 
--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import state from './redux/state';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App state={state} />
+    <BrowserRouter>
+      <App state={state} />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
