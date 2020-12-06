63efa16537d37563e3e36ec79f3c04a7aa777f75
Igor <testdeveloper7967@gmail.com>
2020-12-06 18:14:40 +0300

    52. Side Effect

--- react-tutorial/src/components/Users/Users.jsx
+++ react-tutorial/src/components/Users/Users.jsx
@@ -4,14 +4,17 @@ import * as axios from "axios";
 import userPhoto from '../../assets/images/default_user.png';
 
 const Users = (props) => {
-    if (props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUsers(response.data.items);
-        });
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+                props.setUsers(response.data.items);
+            });
+        }
     }
 
     return (
         <div>
+            <button onClick={getUsers}>Get Users</button>
             {
                 props.users.map(u => <div key={u.id}>
                     <span>
