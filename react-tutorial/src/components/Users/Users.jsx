cd0912938f7466c1001ff1707df982e7ba0d3092
Igor <testdeveloper7967@gmail.com>
2020-12-07 19:04:44 +0300

    56. Презентационная и Контейнерная компоненты

--- react-tutorial/src/components/Users/Users.jsx
+++ react-tutorial/src/components/Users/Users.jsx
@@ -1,39 +1,25 @@
-import React from 'react';
-import s from './Users.module.css';
-import * as axios from "axios";
-import userPhoto from '../../assets/images/default_user.png';
+import React from "react";
+import s from "./Users.module.css";
+import userPhoto from "../../assets/images/default_user.png";
 
-class Users extends React.Component {
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items);
-            this.props.setTotalUsersCount(response.data.totalCount);
-        });
+let Users = (props) => {
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
     }
-
-    onPageChanged = (pageNumber) => {
-        this.props.setCurrentPage(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items);
-        });
-    }
-
-    render() {
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
-        let pages = [];
-        for(let i = 1; i <= pagesCount; i++){
-            pages.push(i);
-        }
-        return (
+    return (
+        <div>
             <div>
-                <div>
-                    {pages.map(p => {
-                        return <span onClick={() => {this.onPageChanged(p);}} className={this.props.currentPage === p && s.selectedPage}>{p} </span>
-                    })}
+                {pages.map(p => {
+                    return <span onClick={() => {
+                        props.onPageChanged(p);
+                    }} className={props.currentPage === p && s.selectedPage}>{p} </span>
+                })}
 
-                </div>
-                {
-                    this.props.users.map(u => <div key={u.id}>
+            </div>
+            {
+                props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
                             <img className={s.userPhoto} src={u.photos.small != null ? u.photos.small : userPhoto} alt="User photo"/>
@@ -42,15 +28,15 @@ class Users extends React.Component {
                             {
                                 u.followed
                                     ? <button onClick={() => {
-                                        this.props.unfollow(u.id)
+                                        props.unfollow(u.id)
                                     }}>Unfollow</button>
                                     : <button onClick={() => {
-                                        this.props.follow(u.id)
+                                        props.follow(u.id)
                                     }}>Follow</button>
                             }
                         </div>
                     </span>
-                        <span>
+                    <span>
                         <span>
                             <div>
                                 {u.name}
@@ -68,11 +54,10 @@ class Users extends React.Component {
                             </div>
                         </span>
                     </span>
-                    </div>)
-                }
-            </div>
-        );
-    }
+                </div>)
+            }
+        </div>
+    );
 }
 
 export default Users;
--- react-tutorial/src/redux/users-reducer.js
+++ react-tutorial/src/redux/users-reducer.js
@@ -6,9 +6,9 @@ const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
 
 let initialState = {
     users: [],
-    pageSize: 5,
+    pageSize: 20,
     totalUsersCount: 0,
-    currentPage: 5
+    currentPage: 1
 };
 
 const usersReducer = (state = initialState, action) => {
