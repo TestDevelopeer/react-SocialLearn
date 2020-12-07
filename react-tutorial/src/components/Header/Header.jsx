383941a0d5332cb4be850c3a435e78bebeb416ac
Igor <testdeveloper7967@gmail.com>
2020-12-07 23:42:22 +0300

    61. cookie, login в теории, auth/me

--- react-tutorial/src/components/Header/Header.jsx
+++ react-tutorial/src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import s from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className={s.header}>
             <img src="https://upload.wikimedia.org/wikipedia/commons/8/85/Logo-Test.png" alt="Logo" />
+
+            <div className={s.loginBlock}>
+                {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
+            </div>
         </header>
     );
 }
