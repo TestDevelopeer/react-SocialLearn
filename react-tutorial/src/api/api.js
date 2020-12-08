5db98cb128ae6dc9abd7c293637a0a8b8f608c6a
Igor <testdeveloper7967@gmail.com>
2020-12-08 13:14:05 +0300

    63. Практика, DAL, axios.create

---
+++ react-tutorial/src/api/api.js
@@ -0,0 +1,16 @@
+import * as axios from "axios";
+
+const instance = axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    withCredentials: true,
+    headers: {
+        'API-KEY': '9358a999-e675-463d-b4f6-2f8378a87dfa'
+    }
+});
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => response.data);
+    }
+}
